78b0cfb360613fcc9459fdbdfc9f1a318d6d0951
vkray <vitalikkrainyuk@yandex.ru>
2023-06-21 17:11:00 +0300

    master. Вводное в Maven и Spring Boot + web

---
+++ src/main/java/pro/sky/blackwolncalculatordemo/BlackwolnCalculatorDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.blackwolncalculatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackwolnCalculatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackwolnCalculatorDemoApplication.class, args);
+	}
+
+}
